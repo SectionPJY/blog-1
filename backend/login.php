e3298b21ad1d071e70800b0890cfc86bf06ce1d0
SectionPJY <84220538+SectionPJY@users.noreply.github.com>
2021-06-05 00:26:08 +0900

    Add files via upload

---
+++ backend/login.php
@@ -0,0 +1,31 @@
+<?php
+
+	session_start();
+
+	$conn = mysqli_connect("localhost", "root", "vudrkd1004", "blog");	/* MySQL PHP 연동 */
+	$conn->set_charset("utf8");
+
+	function mq($sql) {
+		global $conn;
+		return $conn->query($sql);
+	}
+?>
+
+
+<?php
+	$id =  $_GET['id'];
+	$password =  $_GET['password'];
+
+	$sql = mysqli_query($conn, "SELECT * FROM user WHERE id='".$id."'") or die ("알 수 없는 오류");
+	$member = $sql->fetch_array();
+	$hash_pwd = $member['password'];
+
+	if(password_verify($pwd, $hash_pwd)) {
+		$_SESSION['id'] = $member["id"];
+		$_SESSION['name'] = $member["name"];
+
+		echo "<script> alert('로그인 되었습니다'); location.href='./index.php'; </script>";
+	} else {
+		echo "<script> alert('아이디 또는 비밀번호를 확인해주세요.'); history.back(); </script>";
+	}
+?>
